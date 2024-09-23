727dd4e6eda02ae9ab0b96ab3364e4804fe278c0
luiz0ar <161396940+luiz0ar@users.noreply.github.com>
2024-09-23 20:34:04 -0300

    Atualizar o 7.c

--- 7/7.c
+++ 7/7.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
-int main()
-
-{
-    int valor;
-    int Vvalor;
-
-    printf("Digite o primeiro valor:");
-    scanf("%d", &valor);
-    printf("Digite o segundo valor:");
-    scanf("%d", &Vvalor);
-
-    if(Vvalor > valor) { ;
-        printf("%d", Vvalor);
+int maior(int a, int b) {
+    if (a > b) {
+        return a;
+    } else {
+        return b;
     }
-    else{
-        printf("%d", valor);
-    }
+}
+
+int main() {
+    int num1, num2;
+    
+    printf("Digite o primeiro número: ");
+    scanf("%d", &num1);
+    
+    printf("Digite o segundo número: ");
+    scanf("%d", &num2);
+    
+    printf("O maior número é: %d\n", maior(num1, num2));
+    
     return 0;
 }
